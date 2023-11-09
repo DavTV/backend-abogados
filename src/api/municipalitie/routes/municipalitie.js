cd318cb056aaf05356238674498d48fd417c4e8e
DavTV <atellovalqui@gmail.com>
2023-11-09 18:40:17 -0500

    Primera carga del Backend

---
+++ src/api/municipalitie/routes/municipalitie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * municipalitie router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::municipalitie.municipalitie');
---
+++ src/api/municipalitie/services/municipalitie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * municipalitie service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::municipalitie.municipalitie');
