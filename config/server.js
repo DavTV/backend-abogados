f19345279346661fb405aa5c5b90817017d66af2
DavTV <atellovalqui@gmail.com>
2023-12-09 18:58:58 -0500

    Agregando arreglo de api_keys al archivo server.js

--- config/server.js
+++ config/server.js
@@ -2,6 +2,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS',["YEu3P3NQW07cA2yFZc/27g==","w6b2jfkAjEKjIkjeBDaPBw==","P/351K7UPCBd+ixftxqQ/w=="]),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
