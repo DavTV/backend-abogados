9209f58e18edbdb20e8c1fd9858c6b231b245f2e
DavTV <atellovalqui@gmail.com>
2023-12-09 19:08:48 -0500

    Fijando cedenciales en config.js

--- config/plugins.js
+++ config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'cloudinary',
         providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+          cloud_name:'dge2mmnv4',
+          api_key: '911739271767558',
+          api_secret: 'UeioctfaO7yg2OiHRGcE8NW6VlA',
         },
         actionOptions: {
           upload: {},
