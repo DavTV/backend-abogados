287f1715bd99a3aa2119c8c6220155e892bf7943
DavTV <atellovalqui@gmail.com>
2023-12-09 19:13:59 -0500

    Actualizando archivo config.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,9 @@
+const crypto = require('crypto');
+
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name:'dge2mmnv4',
-          api_key: '911739271767558',
-          api_secret: 'UeioctfaO7yg2OiHRGcE8NW6VlA',
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
     },
-    // ...
-  });
+  },
+});
